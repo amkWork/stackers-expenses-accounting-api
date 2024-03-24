3659fd4b835b96e95b12911f94a3cdba090ce75d
amkWork <a.m.kamaletdinov@it42.ru>
2024-03-24 12:07:35 +0300

    Add Operation admin configuration

--- apps/operation/admin.py
+++ apps/operation/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+from .models import Operation
+
+
+@register(Operation)
+class OperationAdmin(ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    list_filter = ('name', 'created_at',)
+    search_fields = ('name',)
+    ordering = ('-created_at',)
