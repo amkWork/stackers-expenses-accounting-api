872fc936dc97dd61653de3e3d8024ac044847ff0
amkWork <a.m.kamaletdinov@it42.ru>
2024-03-24 20:38:56 +0300

    Update operation

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -1,11 +1,12 @@
 from django.db.models import DateTimeField, FloatField, Model, TextField
+from django.utils import timezone
 
 
 class Operation(Model):
     name = TextField(verbose_name='Name')
     description = TextField(null=True, verbose_name='Description')
     value = FloatField(verbose_name='Amount of money')
-    created_at = DateTimeField(verbose_name='Perform date & time')
+    created_at = DateTimeField(default=timezone.now, verbose_name='Perform date & time')
 
     class Meta:
         verbose_name = 'Operation'
