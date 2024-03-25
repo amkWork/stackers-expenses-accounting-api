fafce213bf466d910a28754572abeec3022068fc
amkWork <a.m.kamaletdinov@it42.ru>
2024-03-25 11:46:00 +0300

    Add Category

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -1,5 +1,24 @@
-from django.db.models import DateTimeField, FloatField, Model, TextField
+from django.db.models import (
+    SET_NULL,
+    CharField,
+    DateTimeField,
+    FloatField,
+    ForeignKey,
+    Model,
+    TextField,
+)
 from django.utils import timezone
+
+
+class Category(Model):
+    name = CharField(max_length=64, verbose_name='Name')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Operation(Model):
@@ -7,6 +26,14 @@ class Operation(Model):
     description = TextField(null=True, verbose_name='Description')
     value = FloatField(verbose_name='Amount of money')
     created_at = DateTimeField(default=timezone.now, verbose_name='Perform date & time')
+
+    category = ForeignKey(
+        Category,
+        null=True,
+        on_delete=SET_NULL,
+        related_name='operations',
+        verbose_name='Category',
+    )
 
     class Meta:
         verbose_name = 'Operation'
