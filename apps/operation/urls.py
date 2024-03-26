67d75c4b7eafe5f7362415f36a7e168ff2370ef1
amkWork <a.m.kamaletdinov@it42.ru>
2024-03-26 17:26:04 +0300

    Implement all operations retrieval endpoint

--- apps/operation/urls.py
+++ apps/operation/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+
+from .views import get_all_operations
+
+
+urlpatterns = [
+    path('all/', view=get_all_operations),
+]
--- apps/operation/views.py
+++ apps/operation/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from http import HTTPStatus
+
+from django.db.models import F
+from django.http import HttpRequest, JsonResponse
+
+from .models import Operation
 
 
-# Create your views here.
+def get_all_operations(request: HttpRequest) -> JsonResponse:
+    offset: str = request.GET.get('offset', '0')
+    limit: str = request.GET.get('limit', '20')
+
+    if not offset.isdigit() or not limit.isdigit():
+        return JsonResponse(
+            {'message': 'Pagination parameters are not valid numbers.'},
+            status=HTTPStatus.BAD_REQUEST,
+        )
+
+    offset = int(offset)
+    limit = int(limit)
+
+    operations = Operation.objects.order_by('-created_at')
+
+    if category_id := request.GET.get('category-id'):
+        operations = operations.filter(category=category_id)
+
+    operations = operations[offset:offset + limit]
+
+    operations = operations.annotate(category_name=F('category__name'))
+
+    return JsonResponse({
+        'operations': list(operations.values()),
+    })
