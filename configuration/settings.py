94107e7d6ebce6eb24533ecd684b3769064e986a
amkWork <a.m.kamaletdinov@it42.ru>
2024-03-27 14:19:57 +0300

    Set up Swagger

--- configuration/settings.py
+++ configuration/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +76,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'configuration.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 
 # Database
--- configuration/urls.py
+++ configuration/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg.openapi import Info
+from drf_yasg.views import get_schema_view
+
+
+swagger_view = get_schema_view(
+    info=Info(
+        title='API documentation',
+        default_version='0.0.1',
+    ),
+)
 
 
 urlpatterns = [
     path('admin/', view=admin.site.urls),
+    path('swagger/', view=swagger_view.with_ui('swagger', cache_timeout=0)),
 
     path('goal/', view=include('apps.goal.urls')),
     path('operation/', view=include('apps.operation.urls')),
